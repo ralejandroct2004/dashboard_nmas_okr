fb408b40e6335096831a30068891e182b1d6b38a
inesrendon13-star <ines.rendon.13@gmail.com>
2026-05-01 00:15:48 -0600

    first_commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import dash
+from dash import html
+import dash_bootstrap_components as dbc
+
+app = dash.Dash(
+    __name__,
+    use_pages=True,
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    title="Laines Dashboard",
+)
+
+app.layout = html.Div(
+    className="dashboard-wrapper",
+    children=[
+        dash.page_container,
+    ],
+)
+
+server = app.server  # necesario para Render/Gunicorn
+
+if __name__ == "__main__":
+    app.run(debug=True)
